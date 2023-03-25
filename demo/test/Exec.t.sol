69ea3889064d5164a89fab19763f6c4b42ca1a93
wighawag <wighawag@gmail.com>
2023-03-25 19:26:11 +0000

    show it work as a setup

--- demo/test/Exec.t.sol
+++ demo/test/Exec.t.sol
@@ -6,9 +6,18 @@ import "forge-std/Test.sol";
 import {Exec} from "forge-exec/Exec.sol";
 
 contract ExecTest is Test {
-    function setUp() public {}
-
-    function testGet() public {
+    function setUp() public {
+        // we can for example make reuse of a deploy script here and setup our test environment the same way we could do a script, all in js.
         Exec.execute("./example.js");
+    }
+
+    function testDeplyment() public {
+        bytes memory code = 0x90193C961A926261B756D1E5bb255e67ff9498A1.code;
+        assert(code.length > 0);
+    }
+
+    function testDeplymentAgain() public {
+        bytes memory code = 0x90193C961A926261B756D1E5bb255e67ff9498A1.code;
+        assert(code.length > 0);
     }
 }
