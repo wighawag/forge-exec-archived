6414a0444401a8d3e71cb5dc5e4eaa7b2492d9ba
wighawag <wighawag@gmail.com>
2023-03-26 08:46:45 +0100

    wip rus based ipc client

---
+++ forge-ipc-client/README.md
@@ -0,0 +1,3 @@
+# forge-ipc-client
+
+ipc client that let forge to connect to an ipc server while executing solidity
---
+++ forge-ipc-client/src/main.rs
@@ -0,0 +1,55 @@
+// fn main() {
+//     println!("Hello, world!");
+    
+// }
+
+use interprocess::local_socket::{LocalSocketStream, NameTypeSupport};
+
+use std::io::{prelude::*, BufReader};
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("Hello, world!");
+// Pick a name. There isn't a helper function for this, mostly because it's largely unnecessary:
+// in Rust, `match` is your concise, readable and expressive decision making construct.
+let name = {
+     // This scoping trick allows us to nicely contain the import inside the `match`, so that if
+     // any imports of variants named `Both` happen down the line, they won't collide with the
+     // enum we're working with here. Maybe someone should make a macro for this.
+     use NameTypeSupport::*;
+     match NameTypeSupport::query() {
+         OnlyPaths | Both => "/tmp/app.world",
+         OnlyNamespaced => "@app.world",
+     }
+};
+
+println!("connecting to {}", name);
+
+// Preemptively allocate a sizeable buffer for reading.
+// This size should be enough and should be easy to find for the allocator.
+let mut buffer = String::with_capacity(128);
+
+// Create our connection. This will block until the server accepts our connection, but will fail
+// immediately if the server hasn't even started yet; somewhat similar to how happens with TCP,
+// where connecting to a port that's not bound to any server will send a "connection refused"
+// response, but that will take twice the ping, the roundtrip time, to reach the client.
+let conn = LocalSocketStream::connect(name)?;
+// Wrap it into a buffered reader right away so that we could read a single line out of it.
+let mut conn = BufReader::new(conn);
+
+// Write our message into the stream. This will finish either when the whole message has been
+// writen or if a write operation returns an error. (`.get_mut()` is to get the writer,
+// `BufReader` doesn't implement a pass-through `Write`.)
+// conn.get_mut().write_all(b"{\"type\":\"message\",\"data\":{\"type\":\"response\",\"data\":\"0xFF\"}}\n")?; // \f = \x0c
+conn.get_mut().write_all(b"{\"type\":\"message\",\"data\":{\"type\":\"terminate\",\"error\":\"Terminate Now!\"}}\n")?; // \f = \x0c
+
+// // We now employ the buffer we allocated prior and read a single line, interpreting a newline
+// // character as an end-of-file (because local sockets cannot be portably shut down), verifying
+// // validity of UTF-8 on the fly.
+conn.read_line(&mut buffer)?;
+
+// // Print out the result, getting the newline for free!
+print!("Server answered: {buffer}");
+
+Ok(())
+}
