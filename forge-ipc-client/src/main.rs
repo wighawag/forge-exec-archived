f3aaae812a95b77d7e8fdaa73021188c58f4e811
wighawag <wighawag@gmail.com>
2023-03-27 14:00:11 +0100

    use interprocess 1.2.1

--- forge-ipc-client/src/main.rs
+++ forge-ipc-client/src/main.rs
@@ -1,6 +1,5 @@
 
 use std::io::{prelude::*, BufReader};
-use std::fs::OpenOptions;
 use std::error::Error;
 use std::{env, str};
 use std::process::{Command, Stdio};
@@ -8,6 +7,10 @@ use std::{thread, time};
 use ethabi::{encode, Token};
 use hex;
 use rand::prelude::*;
+
+#[cfg(debug_assertions)]
+use std::fs::OpenOptions;
+
 
 use interprocess::local_socket::{LocalSocketStream, NameTypeSupport};
 
@@ -49,6 +52,8 @@ fn connect_and_send(name: &str, retry: u32, retry_interval: u64, message_buffer:
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+
+#[cfg(debug_assertions)]
 let mut file = OpenOptions::new()
 .append(true)
 .open(".rust-executor.log")
@@ -58,6 +63,7 @@ let mut file = OpenOptions::new()
 let args: Vec<String> = env::args().collect();
 let command = &args[1];
 
+#[cfg(debug_assertions)]
 writeln!(file, "{}", args.join(","))?;
 
 if command.eq("init") {
@@ -79,28 +85,31 @@ if command.eq("init") {
     }
     program_args.push(&name);
 
+    #[cfg(debug_assertions)]
     writeln!(file, "spawn {}", program)?;
 
-    let child = Command::new(program)
+    Command::new(program)
         // .stdin(Stdio::null())
         .stdout(Stdio::null())
         .stderr(Stdio::null())
     .args(program_args)
     .spawn()
-
     .expect("failed to execute child");
 
-    writeln!(file, "child PID {}", child.id())?;
+    // #[cfg(debug_assertions)]
+    // writeln!(file, "child PID {}", child.id())?;
 
     // we attempt to connect, we retry 300 times with an interval of 10ms
     // the process has 3 seconds to establish an IPC server
     // this is plenty of time
     connect(&name, 300, 10);
 
+    #[cfg(debug_assertions)]
     writeln!(file, "connected!")?;
     
     print!("0x{}", hex::encode(encode(&[Token::String(String::from(name))])));
 
+    #[cfg(debug_assertions)]
     writeln!(file,"------------------ INIT ------------------")?;
 } else {
     let name = args[2].as_str();
@@ -109,7 +118,10 @@ if command.eq("init") {
         // print!("{}", data);
         let request = connect_and_send(name, 0, 10, data.as_bytes())?;
         print!("{}", request);
+
+        #[cfg(debug_assertions)]
         writeln!(file,"NEW REQUEST: {}", request)?;
+        #[cfg(debug_assertions)]
         writeln!(file,"------------------ EXEC ------------------")?;
     } else if command.eq("terminate") {
         let error_message = match args.len() > 3 {
@@ -121,8 +133,11 @@ if command.eq("init") {
         msg.push_str(error_message);
         connect_and_send(name, 0,10,msg.as_bytes())?;
         print!("0x");
+
+        #[cfg(debug_assertions)]
         writeln!(file,"------------------ TERMINATE ------------------")?;
     } else {
+        #[cfg(debug_assertions)]
         writeln!(file,"------------------ UNKNOWN ------------------")?;
     }
 }
