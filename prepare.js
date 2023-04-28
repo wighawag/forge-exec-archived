68f23f1bbeee7d38f60faaa66ac5caae94b33d3f
wighawag <wighawag@gmail.com>
2023-04-28 20:55:16 +0100

    we move into npm bin folder

--- prepare.js
+++ prepare.js
@@ -2,6 +2,9 @@
 const fs = require('fs');
 if (fs.existsSync("node_modules/forge-exec-ipc-client")) {
     fs.chmodSync("node_modules/forge-exec-ipc-client/bin/forge-exec-ipc-client", 0764);
+    fs.mkdirSync("node_modules/.bin", {recursive: true});
+    fs.copyFileSync("node_modules/forge-exec-ipc-client/bin/forge-exec-ipc-client", "node_modules/.bin")
+    fs.chmodSync("node_modules/.bin/forge-exec-ipc-client", 0764);
 } else {
     console.error(`Please install "forge-exec-ipc-client", this contains the executable needed for forge-exec to operate`)
 }
