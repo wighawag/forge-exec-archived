8c1b1cd3ef33910ecf80768573ec76a663295d34
wighawag <wighawag@gmail.com>
2023-04-28 20:50:09 +0100

    fix

--- prepare.js
+++ prepare.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
 if (fs.existsSync("node_modules/forge-exec-ipc-client")) {
-    fs.chmodSync("node_modules/forge-exec-ipc-client/bin/forge-exec-ipc-client", "u+x");
+    fs.chmodSync("node_modules/forge-exec-ipc-client/bin/forge-exec-ipc-client", 0764);
 } else {
     console.error(`Please install "forge-exec-ipc-client", this contains the executable needed for forge-exec to operate`)
 }
