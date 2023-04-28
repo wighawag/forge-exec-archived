f0b2218a0246e7f75f69269b989db654ee55c2ce
wighawag <wighawag@gmail.com>
2023-04-28 21:08:42 +0100

    use relative path

--- prepare.js
+++ prepare.js
@@ -7,9 +7,11 @@ if (fs.existsSync("node_modules/forge-exec-ipc-client")) {
     const folder_path = path.resolve('node_modules/forge-exec-ipc-client/bin');
     const file_path = path.join(folder_path, 'forge-exec-ipc-client');
     const content = fs.readFileSync(file_path, 'utf-8');
-    const new_path = 'node_modules/.bin/forge-exec-ipc-client';
-    fs.writeFileSync(new_path, content.replace(`$DIR/\${TARGET}/forge-exec-ipc-client`, path.join(folder_path, `\${TARGET}/forge-exec-ipc-client`)))
-    fs.chmodSync(new_path, 0764);
+    const new_folder_path = 'node_modules/.bin/';
+    const new_file_path = path.join(new_folder_path, 'forge-exec-ipc-client');
+    const relative_path = path.relative(new_folder_path, folder_path);
+    fs.writeFileSync(new_file_path, content.replace(`$DIR/\${TARGET}/forge-exec-ipc-client`, path.join(relative_path, `\${TARGET}/forge-exec-ipc-client`)))
+    fs.chmodSync(new_file_path, 0764);
 } else {
     console.error(`Please install "forge-exec-ipc-client", this contains the executable needed for forge-exec to operate`)
 }
